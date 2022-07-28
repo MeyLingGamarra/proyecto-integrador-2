982984f9924c5d2af69fa51d372ccc856a88ea5b
MeyLingGamarra <gamarrameyling@gmail.com>
2022-07-28 16:50:36 -0300

    actualizado

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const menuItems = document.querySelectorAll('.menu-item');
 const menuBtn = document.querySelector('#menu-button');
 
 menuBtn.addEventListener('click', function() {
-    document.body.classList.toggle('mobile-menu_active')
+    document.body.classList.toggle('mobile-menu-active')
 });
 
 menuItems.forEach (function(item) {
